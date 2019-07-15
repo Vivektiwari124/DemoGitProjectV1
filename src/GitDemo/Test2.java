30d61a3c7077d49b6ff745c1b8fcd0b4bd578cf9
vivek.tiwari <vivek.tiwari@Vivek-Tiwari1.diasparkindia.com>
2019-07-15 19:39:54 +0530

    I have added addditional check

--- src/GitDemo/Test2.java
+++ src/GitDemo/Test2.java
@@ -4,7 +4,7 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		String hungry = "yes";
-		if (hungry == "Yes")
+		if (hungry == "Yes" || hungry == "y")
 			System.out.println("eat Something");
 		else {
 			System.out.println("Do Work...");
